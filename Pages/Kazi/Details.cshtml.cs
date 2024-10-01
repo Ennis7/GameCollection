25a92ea10e6e3551842cbdb7ebffa453ea04bd68
kazi-a <kazi.arafat@hotmail.com>
2024-10-01 14:07:13 -0400

    kazi

---
+++ Pages/Kazi/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using GameCollection.Data;
+using GameCollection.Models;
+
+namespace GameCollection.Pages.Kazi
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly GameCollection.Data.GameCollectionContext _context;
+
+        public DetailsModel(GameCollection.Data.GameCollectionContext context)
+        {
+            _context = context;
+        }
+
+      public Games Games { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Games == null)
+            {
+                return NotFound();
+            }
+
+            var games = await _context.Games.FirstOrDefaultAsync(m => m.ID == id);
+            if (games == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Games = games;
+            }
+            return Page();
+        }
+    }
+}
